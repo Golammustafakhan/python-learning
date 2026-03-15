7c1cf75ada64355bf159256de89c8548338ff1b0
Golam Mustafa <golammustafa101112@gmail.com>
2026-03-15 15:20:21 +0530

    Day 2 - variables and data types

---
+++ day_02_variables.py
@@ -0,0 +1,43 @@
+"""
+1. Variables
+2. Data Types (int, float, str, bool)
+3. Type Conversion
+4. f-Strings
+5. None Type
+"""
+
+"""name = "Mustafa"
+age = 22
+
+print(f"My name is {name} and I am {age} years old")"""
+
+
+"""product = "Laptop"
+price = 50000
+quantity = 2
+
+print(f"{product} total price is {price*quantity}")
+"""
+
+
+"""num1 = "15"
+num2 = "5"
+print(int(num1) + int(num2))
+print(int(num1) - int(num2))
+print(int(num1) * int(num2))
+"""
+
+
+"""name = "Mustafa"
+age = 22
+city = "Jagatsinghpur"
+
+print(f"{name} is {age} years old and lived in {city}")
+"""
+
+
+"""birth_year = 2003
+current_year = 2026
+age = current_year - birth_year
+print(f"Mustafa is {age} years old")
+"""
