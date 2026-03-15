5e8f66221c63345a138b01384d4c9c1257338bf3
Golam Mustafa <golammustafa101112@gmail.com>
2026-03-15 15:19:55 +0530

    Day 1 - Python basics

---
+++ day_01_basics.py
@@ -0,0 +1,11 @@
+name = "mustafa"
+age = 22
+city = "jagatsinghpur"
+job = "python dev"
+exprinceYears = 1
+
+print(f"my name is {name}")
+print(f"I am {age}years old")
+print(f"i lived in {city}")
+print(f"i'm a {job}")
+print(f"i have {exprinceYears} in this feild")
