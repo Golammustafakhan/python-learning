cf279b595183333ca1e5f88e41d1d457f40b7695
Golam Mustafa <golammustafa101112@gmail.com>
2026-03-15 15:20:36 +0530

    Day 3 - operators and control flow

---
+++ day_03_control_flow.py
@@ -0,0 +1,55 @@
+"""
+a = 15
+b = 4
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+"""
+
+"""
+x = 10
+y = 20
+print(x == y)
+print(x != y)
+print(x >= y)
+print(x <= y)
+print(x > y)
+print(x < y)
+"""
+
+
+"""
+score = 75
+if score >= 90:
+    print("A")
+elif score >= 75:
+    print("B")
+elif score >= 60:
+    print("C")
+else:
+    print("Fail")
+"""
+
+
+"""
+number = 27
+if number % 2 == 0:
+    print("EVEN")
+else:
+    print("ODD")
+"""
+
+
+"""
+username = "admin"
+password = "1234"
+
+if username == "admin" and password == "1234":
+    print("Login successful")
+elif username == "admin" and password != "1234":
+    print("Wrong password")
+else:
+    print("Invalid user")
+"""
