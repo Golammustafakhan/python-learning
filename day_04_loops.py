3f1ca8c2005a45623e2e6eb49b333368de6fb03b
Golam Mustafa <golammustafa101112@gmail.com>
2026-03-26 11:02:06 +0530

    Day 4 - loops and logic

---
+++ day_04_loops.py
@@ -0,0 +1,51 @@
+"""for i in range(1, 11):
+print(i)
+"""
+
+"""for i in range(1,21):
+    if i % 2 != 0:
+        print(i)
+"""
+
+
+"""for i in range(1,21):
+    if i % 2 != 0:
+        print(i)"""
+
+
+"""word = "python"
+for ch in word:
+    print(ch)
+"""
+
+
+"""for num in range(1, 11):
+    if num == 5:
+        continue
+    print(num)
+"""
+
+
+"""total = 0
+
+for i in range(1, 101):
+    total = total + i
+print(total)
+"""
+
+
+"""nums = [10, 20, 30, 40]
+
+count = 0
+
+for n in nums:
+    count += 1
+print(count)
+"""
+
+
+"""num = 5
+
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+"""
